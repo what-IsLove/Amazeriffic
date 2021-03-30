1b0a789eae02b6bf713c254dc23f963a2705b7af
what-IsLove <ten89658516777@yandex.ru>
2021-03-30 22:16:58 +0300

    p4 modified:   js/app.js  Щелчок на первой вкладке!

--- js/app.js
+++ js/app.js
@@ -2,9 +2,17 @@ var main = function () {
     "use strict";
     $(".tabs a span").toArray().forEach(function (element) {
         $(element).on("click", function () {
+            var $element = $(element);
             $(".tabs a span").removeClass("active");
-            $(element).addClass("active");
-            // $("main .content").empty();
+            $element.addClass("active");
+            $("main .content").empty();
+            if ($element.parent().is(":nth-child(1)")) {
+                console.log("Щелчок на первой вкладке!");
+            } else if ($element.parent().is(":nth-child(2)")) {
+                console.log("Щелчок на второй вкладке!");
+            } else if ($element.parent().is(":nth-child(3)")) {
+                console.log("Щелчок на третьей вкладке!");
+            }
             return false;
         });
     });
