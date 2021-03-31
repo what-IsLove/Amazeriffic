da7572c875003ae4a5fbb2f5f6bab5952fcddead
what-IsLove <ten89658516777@yandex.ru>
2021-03-31 11:43:06 +0300

    p4 some change

--- js/app.js
+++ js/app.js
@@ -1,20 +1,44 @@
 var main = function () {
     "use strict";
+
+    var toDos = [
+        "Закончить писать эту книгу",
+        "Вывести Грейси на прогулку в парк",
+        "Ответить на электронные письма",
+        "Подготовиться к лекции в понедельник",
+        "Обновить несколько новых задач",
+        "Купить продукты"
+    ];
+
     $(".tabs a span").toArray().forEach(function (element) {
         $(element).on("click", function () {
-            var $element = $(element);
+            var $element = $(element),
+                $content;
             $(".tabs a span").removeClass("active");
             $element.addClass("active");
             $("main .content").empty();
             if ($element.parent().is(":nth-child(1)")) {
-                console.log("Щелчок на первой вкладке!");
+                $content = $("<ul>");
+                for (var i = toDos.length; i > -1; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+                $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(2)")) {
-                console.log("Щелчок на второй вкладке!");
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(3)")) {
+                $content = $("<div>");
+                $content.append($("<input>").text(""));
+                $content.append($("<button>").text("+"));
                 console.log("Щелчок на третьей вкладке!");
+                $("main .content").append($content);
             }
             return false;
         });
     });
+    $(".tabs a:first-child span").trigger("click");
 };
 main();
