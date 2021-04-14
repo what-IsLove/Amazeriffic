c79f41da23fee169b09f0def7754545bbeaf2080
what-IsLove <ten89658516777@yandex.ru>
2021-04-14 13:25:23 +0300

    p5 tags

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
-var main = function () {
+var main = function (toDoObjects) {
     "use strict";
 
-    var toDos = [
-        "Закончить писать эту книгу",
-        "Вывести Грейси на прогулку в парк",
-        "Ответить на электронные письма",
-        "Подготовиться к лекции в понедельник",
-        "Обновить несколько новых задач",
-        "Купить продукты"
-    ];
+    var toDos = toDoObjects.map(function (toDo) {
+        return toDo.description;
+    });
 
     $(".tabs a span").toArray().forEach(function (element) {
         $(element).on("click", function () {
@@ -24,26 +19,64 @@ var main = function () {
                 };
                 $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(2)")) {
+                console.log("Щелчок на вкладке Старые")
                 $content = $("<ul>");
                 toDos.forEach(function (todo) {
                     $content.append($("<li>").text(todo));
                 });
                 $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(3)")) {
+                console.log("Щелчок на вкладке Теги");
+
+                var organizedByTag = [
+                    {
+                        "name": "покупки",
+                        "toDos": ["Купить продукты "]
+                    },
+                    {
+                        "name": "рутина",
+                        "toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк "]
+                    },
+                    {
+                        "name": "писательство",
+                        "toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
+                    },
+                    {
+                        "name": "работа",
+                        "toDos": ["Сделать несколько новых задач", "Подготовиться к лекции в понедельник", "Ответить на электронные письма", "Закончить писать книгу"]
+                    },
+                    {
+                        "name": " преподавание",
+                        "toDos": ["Подготовиться к лекции в понедельник"]
+                    },
+                    {
+                        "name": "питомцы",
+                        "toDos": ["Вывести Грейси на прогулку в парк "]
+                    }
+                ];
+                organizedByTag.forEach(function (tag) {
+                    var $tagName = $("<h4>").text(tag.name),
+                        $content = $("<ul>");
+                    $tagName.addClass("tagsH4")
+                    tag.toDos.forEach(function (description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
+
+
+            } else if ($element.parent().is(":nth-child(4)")) {
+                console.log("Щелчок на вкладке Добавить");
                 $content = $("<div>");
                 $content.addClass("tabAddContent");
                 $content.append($("<input>").text(""));
                 $content.append($("<button>").text("+"));
                 $("main .content").append($content);
-
-                $('.tabAddContent').each(function () {
-                    $('.tabAddContent button').on("click", function () {
-                        if($('.tabAddContent input').val()!=""){
-                            toDos.push($('.tabAddContent input').val());
-                            // $('.tabAddContent input').empty();
-                        }
-                        return false;
-                    });
+                $("button").on("click", function () {
+                    toDos.push($("input").val());
+                    $("input").val("");
                 });
             }
             return false;
@@ -51,4 +84,8 @@ var main = function () {
     });
     $(".tabs a:first-child span").trigger("click");
 };
-main();
+$(document).ready(function () {
+    $.getJSON("todos.json", function (toDoObjects) {
+        main(toDoObjects);
+    });
+});
