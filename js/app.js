386ff7db0d97064d8759b0aeedbdd075454dd523
what-IsLove <ten89658516777@yandex.ru>
2021-04-14 14:18:39 +0300

    p5 test+tags

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+var organizeByTags = function (toDoObjects) {
+    var tags = [];
+    toDoObjects.forEach(function (toDo) {
+        toDo.tags.forEach(function (tag) {
+            if (tags.indexOf(tag) === -1) {
+                tags.push(tag);
+            }
+        });
+    });
+
+    var tagObjects = tags.map(function (tag) {
+        var toDosWithTag = toDoObjects.filter(function (toDo) {
+            if (toDo.tags.indexOf(tag) !== -1) {
+                return toDo.description;
+            }
+        }).map(function (toDo) { return toDo.description });
+        return { "name": tag, "toDos": toDosWithTag };
+    });
+    console.log(tagObjects)
+    return tagObjects;
+};
+
 var main = function (toDoObjects) {
     "use strict";
 
@@ -28,32 +50,8 @@ var main = function (toDoObjects) {
             } else if ($element.parent().is(":nth-child(3)")) {
                 console.log("Щелчок на вкладке Теги");
 
-                var organizedByTag = [
-                    {
-                        "name": "покупки",
-                        "toDos": ["Купить продукты "]
-                    },
-                    {
-                        "name": "рутина",
-                        "toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк "]
-                    },
-                    {
-                        "name": "писательство",
-                        "toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
-                    },
-                    {
-                        "name": "работа",
-                        "toDos": ["Сделать несколько новых задач", "Подготовиться к лекции в понедельник", "Ответить на электронные письма", "Закончить писать книгу"]
-                    },
-                    {
-                        "name": " преподавание",
-                        "toDos": ["Подготовиться к лекции в понедельник"]
-                    },
-                    {
-                        "name": "питомцы",
-                        "toDos": ["Вывести Грейси на прогулку в парк "]
-                    }
-                ];
+                var organizedByTag = organizeByTags(toDoObjects);
+
                 organizedByTag.forEach(function (tag) {
                     var $tagName = $("<h4>").text(tag.name),
                         $content = $("<ul>");
@@ -68,16 +66,29 @@ var main = function (toDoObjects) {
 
 
             } else if ($element.parent().is(":nth-child(4)")) {
-                console.log("Щелчок на вкладке Добавить");
-                $content = $("<div>");
-                $content.addClass("tabAddContent");
-                $content.append($("<input>").text(""));
-                $content.append($("<button>").text("+"));
-                $("main .content").append($content);
-                $("button").on("click", function () {
-                    toDos.push($("input").val());
-                    $("input").val("");
+                var $input = $("<input>").addClass("description"),
+                    $inputLabel = $("<p>").text("Новая задача: "),
+                    $tagInput = $("<input>").addClass("tags"),
+                    $tagLabel = $("<p>").text("Тэги: "),
+                    $button = $("<button>").text("+");
+                $button.on("click", function () {
+                    var description = $input.val(),
+                        // разделение в соответствии с запятыми
+                        tags = $tagInput.val().split(",");
+                    toDoObjects.push({ "description": description, "tags": tags });
+                    // обновление toDos
+                    toDos = toDoObjects.map(function (toDo) {
+                        return toDo.description;
+                    });
+                    $input.val("");
+                    $tagInput.val("");
                 });
+                $("main .content").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button);
+            }
+            else if ($element.parent().is(":nth-child(5)")) {
+                var js = document.createElement('script');
+                js.src = "zadanie_app.js";
+                document.body.appendChild(js);
             }
             return false;
         });
---
+++ js/test.js
@@ -0,0 +1,34 @@
+
+var main = function (toDoObjects) {
+    "use strict";
+    var organizeByTags = function (toDoObjects) {
+        var tags = [];
+        toDoObjects.forEach(function (toDo) {
+            toDo.tags.forEach(function (tag) {
+                if (tags.indexOf(tag) === -1) {
+                    tags.push(tag);
+                }
+            });
+        });
+    
+        var tagObjects = tags.map(function (tag) {
+            var toDosWithTag = toDoObjects.filter(function (toDo) {
+                if (toDo.tags.indexOf(tag) !== -1) {
+                    return toDo.description;
+                }
+            }).map(function (toDo) { return toDo.description });
+            return { "name": tag, "toDos": toDosWithTag };
+        });
+        console.log(tagObjects)
+        return tagObjects;
+    };
+    organizeByTags(toDoObjects);
+};
+$(document).ready(function () {
+    console.log("Start");
+    $.getJSON("todos.json", function (toDoObjects) {
+        main(toDoObjects);
+    });
+    
+});
+
