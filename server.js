88325187f7b08e672ed3a09929146f31fec9f5fd
what-IsLove <ten89658516777@yandex.ru>
2021-05-11 17:30:51 +0300

    p7 installed and checked mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express"),
     http = require("http"),
+    mongoose = require("mongoose"),
     app = express(),
     toDos = [
         {
@@ -31,18 +32,54 @@ var express = require("express"),
 app.use(express.static(__dirname + "/client"));
 app.use(express.urlencoded({}));
 
+mongoose.connect('mongodb://localhost/amazeriffic');
+
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [String]
+});
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+
+
 http.createServer(app).listen(3000);
 
 app.get("/todos.json", function (req, res) {
-    res.json(toDos);
+    ToDo.find({}, function (err, toDos) {
+        res.json(toDos);
+    });
 });
 
+// app.post("/todos", function (req, res) {
+//     // сейчас объект сохраняется в req.body
+//     var newToDo = req.body;
+//     console.log(newToDo);
+//     toDos.push(newToDo);
+//     // отправляем простой объект
+//     res.json({ "message": "Вы размещаетесь на сервере!" });
+// });
+
 app.post("/todos", function (req, res) {
-    // сейчас объект сохраняется в req.body
-    var newToDo = req.body;
-    console.log(newToDo);
-    toDos.push(newToDo);
-    // отправляем простой объект
-    res.json({ "message": "Вы размещаетесь на сервере!" });
+    console.log(req.body);
+    var newToDo = new ToDo({
+        "description": req.body.description,
+        "tags": req.body.tags
+    });
+    newToDo.save(function (err, result) {
+        if (err !== null) {
+            console.log(err);
+            res.send("ERROR");
+        } else {
+            // клиент ожидает, что будут возвращены все задачи,
+            // поэтому для сохранения совместимости сделаем дополнитель) в папку part7 . Нам понадобится файл package.json , гденый запрос
+            ToDo.find({}, function (err, result) {
+                if (err !== null) {
+                    // элемент не был сохранен
+                    res.send("ERROR");
+                }
+                res.json(result);
+            });
+        }
+        2
+    });
 });
 
